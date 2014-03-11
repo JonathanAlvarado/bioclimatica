2fa7ab15d6e539581247f17f3a5a1f03d31eae54
Jonathan Alvarado <jonathan.aam92@gmail.com>
2014-03-11 12:08:26 -0600

    acomodo webapp

--- nom020/nom/views.py
+++ nom020/nom/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def index(request):
+	context = {'title':'prueba', 'content':'prueba'}
+	return render(request, 'nom/index.html', context)
--- nom020/nom020/urls.py
+++ nom020/nom020/urls.py
@@ -13,5 +13,6 @@ urlpatterns = patterns('',
     url( r'^hello/$', hello ),
     url( r'^time/$', current_datetime ),
     url( r'^time/plus/(\d{1,2})/$',hours_ahead ),
-    url( r'^nom/$', calculation ),
+    #url( r'^nom/$', calculation ),
+    url(r'^nom/', include('nom.urls')),
 )
