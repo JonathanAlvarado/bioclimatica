571cbfcde0b5a6267366b346a71256bf340d3590
Jonathan Alvarado <jonathan.aam92@gmail.com>
2014-03-11 21:37:58 -0600

    valores unicos

--- nom020/nom/views.py
+++ nom020/nom/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from nom.models import soluciones
+from nom.models import soluciones, ciudades
 
 # Create your views here.
 def index(request):
 	sol = soluciones.objects.all()
-	context = {'title':'prueba', 'content':'prueba', 'solucion': sol }
+	edos = ciudades.objects.values('estado').distinct()
+	context = {'title':'prueba', 'content':'prueba', 'solucion': sol, 'estados': edos }
 	return render(request, 'nom/index.html', context)
