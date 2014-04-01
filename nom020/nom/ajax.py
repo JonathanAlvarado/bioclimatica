c4b21c6d29fad9dcc4c7a888b89ab7ddcd9c0ceb
Jonathan Alvarado <jonathan.aam92@gmail.com>
2014-04-01 00:57:06 -0600

    chained selects working

--- nom020/nom/ajax.py
+++ nom020/nom/ajax.py
@@ -1,15 +1,26 @@
-from django.utils import simplejson
 from dajaxice.decorators import dajaxice_register
 from dajax.core import Dajax
-
-@dajaxice_register
-def sayhello(request):
-	return simplejson.dumps({'message':'Hello World'})
+from nom.models import ciudades, soluciones, soluciones_detalles, ciudades_k, ciudades_fg, ciudades_temp
+from django.db.models import Q
 
 @dajaxice_register
 def multiply(request, a, b):
 	dajax = Dajax()
 	result = int(a) * int(b)
 	dajax.assign('#result','value',str(result))
+	return dajax.json()
+
+@dajaxice_register
+def update_city(request, option):
+	dajax = Dajax()
+	cities = [['','Escoge una ciudad']]
+	for city in ciudades.objects.all():
+		if city.estado_id == int(option):
+			cities.append([city.id, city.ciudad])
+	options = []
+	for city in cities:
+		options.append( "<option value='%s'>%s</option>" % ( city[0], city[1]) )
+
+	dajax.assign( '#id_ciudad', 'innerHTML', ''.join( options ) )
 	return dajax.json()
 
--- nom020/nom/static/nom/js/main.js
+++ nom020/nom/static/nom/js/main.js
@@ -1,22 +1,19 @@
 $( document ).ready(function() {
 
 	$( "#id_estado" ).on('change', function() {
+		//alert($(this).val());
   		display_cities( $(this).val() );
   	});
   
 })
 
 function display_cities(edo){
-	Dajaxice.nom.update_city( Dajax.process, {'option':edo} );
+	Dajaxice.nom.update_city( Dajax.process, {'option':edo} )
 }
 
-function send_form(){
+/*function send_form(){
     Dajaxice.nom.send_form(Dajax.process,{'form':$('#data_form').serialize(true)});
-}
-
-function my_js_callback(data){
-	alert(data.message);
-}
+}*/
 
 function calculate(){
 	Dajaxice.nom.multiply(Dajax.process,{'a':$('#a').val(),'b':$('#b').val()})
